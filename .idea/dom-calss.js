934fb27510a440e47e54ce301190a9371f00fa77
jinming6568 <443836568@qq.com>
2016-08-11 23:01:19 +0800

    add new file about dom-class

---
+++ .idea/dom-calss.js
@@ -0,0 +1,28 @@
+/**
+ * 通过calss获取DOM元素
+ * author:JM
+ * Released on: August 11, 2016
+ * @param {Object} [arguments[0]=document] - 上级DOM元素
+ * @param {String} arguments[1] - 想要获取的ID
+ * */
+function getClassName(){
+    var oParent = arguments.length == 1?document:arguments[0];
+    var sClass = arguments.length == 1?arguments[0]:arguments[1];
+    if(document.getElementsByClassName){
+        return oParent.getElementsByClassName(sClass);
+    }else{
+        var aTmp = oParent.getElementsByTagName('*');
+        var aRes=[];
+        var arr =[];
+        for(var i=0;i<aTmp.length;i++){
+            arr = aTmp[i].className.split(' ');
+            for (var j=0;j<arr.length;j++){
+                if(arr[j] == sClass)
+                {
+                    aRes.push(aTmp[i]);
+                }
+            }
+        }
+        return aRes;
+    }
+}
