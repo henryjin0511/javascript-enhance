38560a44a2ea4b9648922a7d03add9aa0665e809
jinming6568 <443836568@qq.com>
2016-09-12 12:51:04 +0800

    ajax.js : add param dataType

--- ajax.js
+++ ajax.js
@@ -6,6 +6,7 @@
  * @param {String} [param.type="get"]-post或get不区分大小写
  * @param {String} param.url - 请求地址
  * @param {Object} param.data - {a:1,b:2}
+ * @param {String} [param.dataType = 'json'] - 预期服务器返回的数据类型 {'xml','text','json'}
  * @param {Boolean} [param.async=true] true为异步 false为同步
  * @param {Function} param.success 成功函数返回responseText
  * @param {Function} param.error 失败返回xhr对象
@@ -39,23 +40,12 @@
             }
             return arr.join("&");
         })(param.data);
+        var dataType = param.dataType || 'json';
         var type = param.type || 'GET';
         if(type.toUpperCase() === 'GET'){
             url += url.indexOf("?") == "-1"? "?"+ data:"&"+data;
         }
         var async = typeof param.async === 'undefined'? true : param.async;
-        //异步
-        if(async === true){
-            xhr.onreadystatechange = function(){
-                if(xhr.readyState == 4){
-                    if(xhr.status == 200){
-                        param.success(xhr.responseText);
-                    }else{
-                        param.error(xhr);
-                    }
-                }
-            }
-        }
         xhr.open(type,url,async);
         if(type.toUpperCase() === 'POST'){
             xhr.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
@@ -63,13 +53,41 @@
         }else{
             xhr.send(null);
         }
-        //同步
-        if(async === false) {
-            if (xhr.status == 200) {
-                param.success(xhr.responseText);
-            } else {
+        //异步
+        if(async === true){
+            xhr.onreadystatechange = function(){
+                if(xhr.readyState == 4){
+                    getDataBack(xhr);
+                    xhr = null;
+                }
+            }
+        }else{
+            getDataBack(xhr);
+            xhr = null;
+        }
+        function getDataBack(xhr) {
+            if(xhr.status == 200){
+                if (dataType.toUpperCase() === "TEXT"){
+                    if (param.success != null){
+                        param.success(xhr.responseText); // 普通文本
+                    }
+                }else if(dataType.toUpperCase() === "XML"){
+                    if (param.success != null){
+                        param.success(xhr.responseXML); // 接收xml文档
+                    }
+                }else if (dataType.toUpperCase() === "JSON"){
+                    if (param.success != null) {
+                        try{
+                            param.success(eval("("+xhr.responseText+")")); //将json字符串转换为js对象)
+                        }catch (e){
+                            param.error(xhr.responseText);
+                        }
+                    }
+                }
+            }else{
                 param.error(xhr);
             }
+
         }
     };
     window.ajax = ajax;
