1dbe7791ab86809c293ff90c0b62c1580485cb92
e <443836568@qq.com>
2017-03-22 23:45:46 +0800

    fix bugs in cookie.js

--- cookie.js
+++ cookie.js
@@ -3,23 +3,24 @@
  * 原生操作cookie
  * author:JM
  * Released on: August 15, 2016
+ * Update on：March 22, 2017
  * */
 (function (window,undefined) {
-    var cookie = new Object();
+    var cookie = {};
     /**
      * 设置cookie
      * @param {String} sname - cookie 名
      * @param {String} svalue - cookie 值
-     * @param {String} sdays - cookie 时间 单位：天
-     * @param {String} [arguments[3] = ''] - cookie 域 siteDomain(xx.com)
-     * @param {String} [arguments[4] = '/'] - cookie 路径 URL默认
+     * @param {Number} sdays - cookie 时间 单位：天
+     * @param {String} [sdomain = ''] - cookie 域 siteDomain(xx.com)
+     * @param {String} [spath = '/'] - cookie 路径 URL默认
      * */
-    cookie.set = function(sname,svalue,sdays){
+    cookie.set = function(sname,svalue,sdays,sdomain,spath){
         var date = new Date();
         date.setTime(date.getTime()+sdays*24*60*60*1000);
-        var expires = sdays?'expires='+date.toGMTString():'expires=0;';
-        var domain = arguments[3] ?'domain='+arguments[3]+';':'';
-        var path = arguments[4] ?'path='+arguments[4]+';':'path=/;';
+        var expires = sdays?'expires='+date.toGMTString()+';':'expires=0;';
+        var domain = typeof sdomain !== 'undefined'?'domain='+sdomain+';':'';
+        var path = typeof spath !== 'undefined'?'path='+spath+';':'path=/;';
         document.cookie = sname+'='+encodeURIComponent(svalue)+';'+expires+domain+path;
     };
     /**
